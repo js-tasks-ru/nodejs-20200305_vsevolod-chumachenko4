b9406cd95f25fdabc735b3029e800a29d57783fd
Всеволод <cvs19998@gmail.com>
2020-03-23 23:36:57 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,22 +1,44 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Вложенность папок недопустима');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
+  function giveFile() {
+    const readFileStream = fs.createReadStream(filepath);
 
-      break;
+    readFileStream.on('error', (error) => {
+      console.log(error);
+      if (error.code === 'ENOENT') {
+        res.statusCode = 404;
+        res.statusMessage = 'Not Found';
+        res.end('Файл не найден');
+        return;
+      }
+      res.statusCode = 500;
+      res.statusMessage = 'Server Error';
+      res.end('Серверная ошибка');
+    });
+    readFileStream.pipe(res);
+  }
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  if (req.method === 'GET') {
+    giveFile();
+  } else {
+    res.statusCode = 501;
+    res.end('Not implemented');
   }
 });
 
