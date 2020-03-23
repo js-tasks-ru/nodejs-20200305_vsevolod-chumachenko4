af2157577e1df58d366f382ed31ed52e78f8acbb
Всеволод <cvs19998@gmail.com>
2020-03-23 18:57:59 +0300

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,31 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainder = '';
+    this.hasRemaind = false;
   }
 
   _transform(chunk, encoding, callback) {
+    chunk = chunk.toString();
+    const chunks = chunk.split(os.EOL);
+
+    if (this.hasRemaind) {
+      chunks[0] = this.remainder + chunks[0];
+      this.hasRemaind = false;
+    }
+
+    this.remainder = chunks.splice(chunks.length - 1, 1);
+    if (this.remainder !== '') this.hasRemaind = true;
+
+    for (const chunk of chunks) {
+      this.push(chunk.toString());
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.remainder.toString());
   }
 }
 
